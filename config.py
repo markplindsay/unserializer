2e5f71b41bbc49b06ab80f77b87d35e55e6ef3ec
Mark Lindsay <mark@nervous.org>
2019-07-14 02:02:06 +0000

    Use traefik X-Real-IP

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 bind = '0.0.0.0:5001'
 accesslog = '-'
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
+access_log_format = '%({X-Real-IP}i)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
